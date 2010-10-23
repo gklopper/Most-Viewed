0b26c2e7eea9f47751f2e42483c3689ea1a3c2c3
Grant Klopper <grant.klopper@gmail.com>
2010-10-23 09:56:31 +0100

    Got a rough version working

--- src/main/scala/bootstrap/liftweb/Boot.scala
+++ src/main/scala/bootstrap/liftweb/Boot.scala
@@ -1,11 +1,32 @@
 package bootstrap.liftweb
 
-import net.liftweb.http.LiftRules
 import net.liftweb.sitemap.{SiteMap, Menu}
+import net.liftweb.http.{RewriteResponse, ParsePath, RewriteRequest, LiftRules}
+import net.liftweb.util.NamedPF
 
 class Boot {
+
   def boot {
     LiftRules.addToPackages("com.gu.mostviewed")
+
+    LiftRules.early.append(r => r.setCharacterEncoding("UTF-8"))
+
+    LiftRules.autoIncludeAjax = _ => false
+
+    LiftRules.autoIncludeComet = _ => false
+
+
+    LiftRules.statefulRewrite.prepend(NamedPF("SectionMostViewedRewrite") {
+      case RewriteRequest(
+          ParsePath("mostviewed" :: section :: Nil, _, _,_), _, _) =>
+        RewriteResponse(
+          "index" :: Nil, Map("section" -> section)
+      )
+    })
+
+
+
     LiftRules.setSiteMap(SiteMap(Menu("Home") / "index"))
   }
+  
 }
--- src/main/scala/com/gu/mostviewed/snippet/SectionMostViewed.scala
+++ src/main/scala/com/gu/mostviewed/snippet/SectionMostViewed.scala
@@ -1,5 +1,26 @@
 package com.gu.mostviewed.snippet
 
-class SectionMostViewed {
-  def hello = <h1>Hello World</h1>
+import net.liftweb.util.BindHelpers
+import com.gu.mostviewed.SectionApi
+import xml.{Text, Unparsed, NodeSeq}
+import com.gu.openplatform.contentapi.model.Content
+
+class SectionMostViewed extends BindHelpers{
+
+  def name(xml: NodeSeq) = {
+    bind("section", xml, "name" -> SectionApi.fetch.section.get.webTitle)
+  }
+
+  def mostViewed(xml: NodeSeq) = SectionApi.fetch.mostViewed.flatMap( c => bindContent(xml, c))
+
+  def bindContent(xml: NodeSeq, c: Content) = bind("content", xml, "name" --> c.webTitle,
+    "trail-text" --> Unparsed(c.fields.get.get("trailText").get),
+    AttrBindParam("href", c.webUrl, "href"),
+    "trail-image" --> toTrailImage(c))
+
+  def toTrailImage(c: Content) = c.fields.get.get("thumbnail") match {
+    case Some(url) => <img src={url}/>
+    case None => Text("")
+  }
+
 }
